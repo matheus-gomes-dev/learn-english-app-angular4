5fd2f1e3b687a022479aec702bf5fd7a261e80af
matheus-lima92 <lima.eca@gmail.com>
2018-02-08 01:15:08 -0200

    components created with angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
+import { TopoComponent } from './topo/topo.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent,
+    TopoComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
