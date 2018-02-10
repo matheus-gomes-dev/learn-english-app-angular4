39fb79c4031e1b8240d60cd6012c1acc490dd337
matheus-lima92 <lima.eca@gmail.com>
2018-02-10 02:13:46 -0200

    setting phrases in the panel

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Frase} from '../shared/frase.model'
+import {FRASES} from './frases-mock'
+
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -7,7 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+	public frases: Frase[] = FRASES
+
+  constructor() { 
+  	console.log(this.frases)
+  }
 
   ngOnInit() {
   }
---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,8 @@
+export class Frase {
+
+	//public fraseEng: string
+	//public frasePtBr: string
+
+	//forma resumida de estruturar a classe, com os atributos direto no construtor:
+    constructor(public fraseEng: string, public frasePtBr: string) { }
+}
