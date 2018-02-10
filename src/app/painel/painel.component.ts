1f0dc87caa591d121eefcdb8247ced1ad301a081
matheus-lima92 <lima.eca@gmail.com>
2018-02-10 14:19:24 -0200

    sending user answer with event binding

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,13 +11,28 @@ import {FRASES} from './frases-mock'
 })
 export class PainelComponent implements OnInit {
 
-	public frases: Frase[] = FRASES
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase:'
+  public resposta: string
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
 
   constructor() { 
-  	console.log(this.frases)
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase) 
   }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    //console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    console.log('Verificar resposta: ', this.resposta)
   }
 
 }
