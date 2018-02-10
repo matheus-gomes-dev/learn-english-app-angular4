106ff6409f41dd173ee83d92bad54776e64253d0
matheus-lima92 <lima.eca@gmail.com>
2018-02-10 15:35:57 -0200

    @Input (property binding between components painel and progresso)

--- src/app/progresso/progresso.component.ts
+++ src/app/progresso/progresso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progresso.component.css']
 })
 export class ProgressoComponent implements OnInit {
+
+  //progresso é classe filha de painel
+  //posso usar @Input do angular para trocar informação
+  //entre classes pai/filha	
+  //no arquivo html do painel, devo incluir <app-progresso [progresso]="progresso"></app-progresso>
+  @Input() public progresso: number = 0
+
+  //caso eu queria utilizar um outra variável, exemplo <app-progresso [variavel_x]="progresso"></app-progresso>
+  //@Input ficaria da seguinte forma:
+  //@Input('variavel_x') public progresso: number = 0
 
   constructor() { }
 
